1b4cf4beef124d9c85140b9e33218742ed5102df
zhangyongjie <yongjie_zhang@outlook.com>
2018-04-18 13:52:31 +0800

    0418 13:52

---
+++ js/loginJs.js
@@ -0,0 +1,132 @@
+
+var loginBtn = document.getElementById("login-btn");
+var div = document.getElementById("warning");
+window.location.hash = "loginPage";
+window.oldUrl = window.location.hash;
+var res;
+
+//登录按钮事件
+loginBtn.addEventListener("click", function () {
+    var userName = document.getElementById("username");
+    var passWord = document.getElementById("password");
+    var request = new XMLHttpRequest();
+    var loginData = {
+        "mobile": userName.value,
+        "pwd": passWord.value,
+    };
+    //转换json字符串
+    // var dataJson = JSON.stringify({
+    //     "mobile":"13011072992",    //手机号
+    //     "pwd":"123456",            //密码
+    //     });
+
+
+    //判断用户名密码是否为空
+    if (userName.value == "" || passWord.value == "") {
+        (function showFailMsg(obj) {
+            var top = -45;
+            console.log("进入动画函数");
+            div.innerHTML = "请输入用户名和密码";
+            var time1 = setInterval(function () {
+                top = top + 4;
+                if (top > 100) {
+                    clearInterval(time1);
+                    var time3 = setTimeout(function () {
+                        var nowtop = 100;
+                        var time2 = setInterval(function () {
+                            nowtop = nowtop - 4;
+                            if (nowtop < -45) {
+                                clearInterval(time2);
+                                clearTimeout(time3);
+                            }
+                            div.style.top = nowtop + "px";
+                        }, 13);
+                    }, 1000);
+                }
+                div.style.top = top + "px";
+            }, 13);
+        })();
+    } else {
+        //如果用户名密码不为空则发送请求
+        request.open('POST', 'http://yjhapi.agxx.club/iweb/login/check');
+        request.setRequestHeader("Content-Type", "application/json");
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 & request.status == 200) {
+                res = request.responseText;
+                res = JSON.parse(res.substring(1));
+                //登录成功判断
+                ifLogin(res);
+                console.log(res);
+            }
+        };
+        request.send(JSON.stringify(loginData));
+    }
+}, false);
+
+//判断登录是否成功
+function ifLogin(obj) {
+    if (obj.status == 1) {
+        let navLogin = document.querySelector("#nav-login");
+        let navSetup = document.querySelector("#nav-setup");
+        let navWrite = document.querySelector("#nav-write");
+        navLogin.classList.add("hide");
+        navSetup.classList.add("hide");
+        navWrite.classList.remove("hide");
+        navWrite.classList.add("cur");
+        window.location.hash = "userInfoPage";
+    } else {
+        showFailMsg(obj);
+    }
+}
+
+//登录失败显示登录失败信息
+function showFailMsg(obj) {
+    var top = -45;
+    console.log("进入动画函数");
+    div.innerHTML = obj.info;
+    var time1 = setInterval(function () {
+        top = top + 4;
+        if (top > 100) {
+            clearInterval(time1);
+            var time3 = setTimeout(function () {
+                var nowtop = 100;
+                var time2 = setInterval(function () {
+                    nowtop = nowtop - 4;
+                    if (nowtop < -45) {
+                        clearInterval(time2);
+                        clearTimeout(time3);
+                    }
+                    div.style.top = nowtop + "px";
+                }, 13);
+            }, 1000);
+        }
+        div.style.top = top + "px";
+    }, 13);
+    // window.location.href = "./loginSuccess.html";
+}
+
+//onhashchange事件
+window.addEventListener("hashchange",function(){
+    console.log("进入onhashchange    "+window.location.hash);
+    switch(window.location.hash){
+        case "#userInfoPage" : {
+            let nowPage = this.document.getElementById(window.oldUrl.substring(1));
+            let goPage = this.document.getElementById(window.location.hash.substring(1));
+            window.oldUrl = window.location.hash;
+            console.log(nowPage + goPage);
+            nowPage.classList.remove("cur");
+            nowPage.classList.add("hide");
+            goPage.classList.remove("hide");
+            goPage.classList.add("cur");
+        }
+        case "#loginPage" : {
+            let nowPage = this.document.getElementById(window.oldUrl.substring(1));
+            let goPage = this.document.getElementById(window.location.hash.substring(1));
+            window.oldUrl = window.location.hash;
+            nowPage.classList.remove("cur");
+            nowPage.classList.add("hide");
+            goPage.classList.remove("hide");
+            goPage.classList.add("cur");
+        }
+    }
+},false);
