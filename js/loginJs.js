36aa56ab77efa5d45d60efce4059dce2b6793e3a
zhangyongjie <yongjie_zhang@outlook.com>
2018-04-19 12:25:26 +0800

    注册请求完成

--- js/loginJs.js
+++ js/loginJs.js
@@ -5,51 +5,63 @@ var navloginBtn = document.getElementById("nav-login");
 var div = document.getElementById("warning");
 window.location.hash = "loginPage";
 window.oldUrl = window.location.hash;
-var res;
+
 
 //导航栏登录按钮事件
-navloginBtn.addEventListener("click",function(){
+navloginBtn.addEventListener("click", function () {
     window.location.hash = "loginPage";
-},false);
+}, false);
 
 //导航栏注册按钮事件
 navsigninBtn.addEventListener("click", function () {
     window.location.hash = "signInPage";
-},false);
+}, false);
 
 //注册按钮事件
-signinBtn.addEventListener("click",function(){
-    var signInName = document.getElementById("username-signIn");
+signinBtn.addEventListener("click", function () {
     var signInPwd = document.getElementById("password-signIn");
     var signInPwd2 = document.getElementById("password-signIn2");
-    if(signInPwd.value != signInPwd2.value){
-        showFailMsg("两次输入的密码不一致");
-    }else{
-
+    if (signInPwd.value != signInPwd2.value) {
+        showMsg("两次输入的密码不一致");
+    } else {
+        var res;
+        var request = new XMLHttpRequest();
+        var signInName = document.getElementById("username-signIn");
+        var msgCode = document.getElementById("code-number");
+        var signinData = {
+            "mobile": signInName.value, //  注册手机号
+            "pwd": signInPwd.value, //  注册密码
+            "sms_code": msgCode.value,
+        };
+        request.open('POST', 'http://yjhapi.agxx.club/iweb/regist/index');
+        request.setRequestHeader("Content-Type", "application/json");
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 & request.status == 200) {
+                res = request.responseText;
+                res = JSON.parse(res.substring(1));
+                console.log(res);
+            }
+        };
+        request.send(JSON.stringify(signinData));
     }
-},false);
+}, false);
 
 //登录按钮事件
 loginBtn.addEventListener("click", function () {
     var userName = document.getElementById("username");
     var passWord = document.getElementById("password");
-    var request = new XMLHttpRequest();
-    var loginData = {
-        "mobile": userName.value,
-        "pwd": passWord.value,
-    };
-    //转换json字符串
-    // var dataJson = JSON.stringify({
-    //     "mobile":"13011072992",    //手机号
-    //     "pwd":"123456",            //密码
-    //     });
-
 
     //判断用户名密码是否为空
     if (userName.value == "" || passWord.value == "") {
-        showFailMsg("请输入用户名和密码");
+        showMsg("请输入用户名和密码");
     } else {
         //如果用户名密码不为空则发送请求
+        var res;
+        var request = new XMLHttpRequest();
+        var loginData = {
+            "mobile": userName.value,
+            "pwd": passWord.value,
+        };
         request.open('POST', 'http://yjhapi.agxx.club/iweb/login/check');
         request.setRequestHeader("Content-Type", "application/json");
         request.onreadystatechange = function () {
@@ -78,7 +90,7 @@ function ifLogin(obj) {
         exUserInfo(obj.data);
         window.location.hash = "userInfoPage";
     } else {
-        showFailMsg(obj.info);
+        showMsg(obj.info);
     }
 }
 
@@ -101,7 +113,7 @@ function exUserInfo(obj) {
 }
 
 //登录失败显示登录失败信息
-function showFailMsg(info) {
+function showMsg(info) {
     var top = -45;
     console.log("进入动画函数");
     div.innerHTML = info;
@@ -130,20 +142,6 @@ function showFailMsg(info) {
 window.addEventListener("hashchange", function () {
     console.log("进入onhashchange    " + window.location.hash);
     goPage();
-    // switch (window.location.hash) {
-    //     case "#userInfoPage":
-    //         {
-    //             goPage();
-    //         }
-    //     case "#loginPage":
-    //         {
-    //             goPage();
-    //         }
-    //     case "#signInPage":
-    //         {
-    //             goPage();
-    //         }
-    // }
 }, false);
 
 //页面跳转方法
